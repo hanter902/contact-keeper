656105cfffff321cb671aed042ede354f9958ee8
xuan <dangthanhxuan902@gmail.com>
2020-08-25 23:08:09 +0700

    Alerts and Register User

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -11,6 +11,7 @@ import {
     LOGOUT,
     CLEAR_ERRORS
 } from '../types';
+import axios from 'axios';
 
 const AuthState = props => {
     const initialState = {
@@ -24,15 +25,33 @@ const AuthState = props => {
     // Load User
 
     // Register User
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try{
+            const res = await axios.post('/api/users', formData, config);
+            dispatch({type: REGISTER_SUCCESS, payload: res.data});
+        }catch(err){
+            dispatch({ type: REGISTER_FAIL, payload: err.response.data.msg});
+        }
+    }
 
     // Login User
+    const login = () => console.log('login');
 
     // Logout
+    const logout = () => console.log('logout');
 
     // Clear Errors
+    const clearErrors = () => dispatch({type: CLEAR_ERRORS})
 
     return(
-        <AuthContext.Provider value={{ token: state.token, isAuthenticated: state.isAuthenticated, loading: state.loading, user: state.user, error: state.error }}>
+        <AuthContext.Provider value={{ token: state.token, isAuthenticated: state.isAuthenticated, loading: state.loading, user: state.user, error: state.error, 
+                register, login, logout, clearErrors }}>
             {props.children}
         </AuthContext.Provider>
     )
